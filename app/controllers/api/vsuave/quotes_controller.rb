68394dfe8853a3f10d4d719ae4ec630504472a5a
Luke Chambers <lukenatchambers@gmail.com>
2018-09-25 08:56:43 -0600

    Slack wants a post request

--- app/controllers/api/vsuave/quotes_controller.rb
+++ app/controllers/api/vsuave/quotes_controller.rb
@@ -8,4 +8,8 @@ class Api::Vsuave::QuotesController < ApplicationController
     render json: Quote.find_by_id(params[:id])
   end
 
+  def create
+    render json: Quote.limit(1).order("RANDOM()").first.full_quote
+  end
+
 end
