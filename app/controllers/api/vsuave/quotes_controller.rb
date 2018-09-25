ec6db5cb5877d6b70c593d6af75d0090187a4d62
Luke Chambers <lukenatchambers@gmail.com>
2018-09-25 08:37:24 -0600

    Returns an array. Will fix

--- app/controllers/api/vsuave/quotes_controller.rb
+++ app/controllers/api/vsuave/quotes_controller.rb
@@ -1,7 +1,7 @@
 class Api::Vsuave::QuotesController < ApplicationController
 
   def index
-    render json: Quote.limit(1).order("RANDOM()").full_quote
+    render json: Quote.limit(1).order("RANDOM()").first.full_quote
   end
 
   def show
